9a70de815993e7d678ad375e4a76113d5f02edf6
bbaktaeho <lth9036@gmail.com>
2020-06-25 21:20:36 +0900

    Update: All Files

---
+++ react-front/src/routes/Detail.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const { location, history } = this.props;
+        if (!location.state) history.push('/');
+    }
+
+    render() {
+        const { location } = this.props;
+        if (location.state) return <span>{location.state.title}</span>;
+        else return null;
+    }
+}
+
+export default Detail;
